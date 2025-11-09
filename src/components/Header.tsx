f51cd97f7b45181e255e045eb8aa07e7e12b10f4
HazarBakir <hazar_bakir@protonmail.com>
2025-11-10 01:23:36 +0300

    feat: add Header component with styling
    - Introduced a new Header component in Header.tsx, featuring a logo and title.
    - Created corresponding Header.css file to style the header with a flexible layout and backdrop filter.

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <a href="/">
+        <img src="/papyr-logo.png" alt="Papyr Logo" className="header__logo" />
+        <span className="header__title">Papyr.io</span>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
